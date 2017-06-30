35e2e57560efbadc8897117b767359d56b8084e8
changbin <changbin@ouyeel.com>
2017-06-30 11:10:06 +0800

    更新JedisPoolConfig配置

--- Test26-Security/src/main/java/org/sang/Config.java
+++ Test26-Security/src/main/java/org/sang/Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Created by Administrator on 2017/6/30.
@@ -14,21 +15,104 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 public class Config {
     @Value("${spring.redis.host}")
     private String host;
-
     @Value("${spring.redis.password}")
     private String password;
-
     @Value("${spring.redis.port}")
     private int port;
+    @Value("${spring.redis.pool.max-active}")
+    private int maxActive;
+    @Value("${spring.redis.pool.max-wait}")
+    private int maxWait;
+    @Value("${spring.redis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.redis.pool.min-idle}")
+    private int minIdle;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getMaxActive() {
+        return maxActive;
+    }
+
+    public void setMaxActive(int maxActive) {
+        this.maxActive = maxActive;
+    }
+
+    public int getMaxWait() {
+        return maxWait;
+    }
+
+    public void setMaxWait(int maxWait) {
+        this.maxWait = maxWait;
+    }
+
+    public int getMaxIdle() {
+        return maxIdle;
+    }
+
+    public void setMaxIdle(int maxIdle) {
+        this.maxIdle = maxIdle;
+    }
+
+    public int getMinIdle() {
+        return minIdle;
+    }
+
+    public void setMinIdle(int minIdle) {
+        this.minIdle = minIdle;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    @Bean
+    public JedisPoolConfig poolConfig(){
+        JedisPoolConfig poolConfig=new JedisPoolConfig();
+        poolConfig.setMaxTotal(this.maxActive);
+        poolConfig.setMaxWaitMillis(this.maxWait);
+        poolConfig.setMaxIdle(this.maxIdle);
+        poolConfig.setMinIdle(this.minIdle);
+        return poolConfig;
+    }
 
     @Bean
     public JedisConnectionFactory connectionFactory() {
        JedisConnectionFactory connectionFactory=new JedisConnectionFactory();
-        System.out.print(host);
-        System.out.print(password);
-        System.out.print(port+"-----------------------------------");
-        connectionFactory.setHostName(host);
-        connectionFactory.setPassword(password);
+//        System.out.print(this.host);
+//        System.out.print(this.password);
+//        System.out.print(port+"-----------------------------------");
+        connectionFactory.setHostName(this.host);
+        connectionFactory.setPassword(this.password);
+        connectionFactory.setPort(this.port);
         return connectionFactory;
     }
 }
